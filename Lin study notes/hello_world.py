2c5eece9aa50dbf8c73672ce40282e76b79cdce2
linyang1987 <ly132.ad@gmail.com>
2019-12-22 18:51:00 +0000

    2019.12.22 study note

--- Lin study notes/hello_world.py
+++ Lin study notes/hello_world.py
@@ -1,2 +1,10 @@
 if __name__ == '__main__':
-    print("Hello World!")
+    import datetime as dt
+    import my_python_library as my_py
+
+    dt_time = dt.datetime.now()
+
+    print(dt_time)
+    my_py.utils1.my_func1()
+    my_py.my_func2()
+
---
+++ my_python_library/__init__.py
@@ -0,0 +1,2 @@
+import my_python_library.utils1 as utils1
+from my_python_library.utils2 import *
